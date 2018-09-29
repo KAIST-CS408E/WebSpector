e7385750ed2760769f9ec89a05f6e08c564436be
Park jihee <j31d0@kaist.ac.kr>
2018-09-29 21:17:23 +0900

    inject once in many js load

--- instrument/prev_instrument.js
+++ instrument/prev_instrument.js
@@ -1,146 +1,150 @@
-var monitorD = {
-    'window' : {
-        'navigator' : {
-            'appCodeName' : true,
-            'appName' : true,
-            'appVersion' : true,
-            'cookieEnabled' : true,
-            'geolocation' : true,
-            'language' : true,
-            'languages' : true,
-            'onLine' : true,
-            'oscpu' : true,
-            'platform' : true,
-            'product' : true,
-            'productSub' : true,
-            'userAgent' : true,
-            'vendorSub' : true,
-            'vendor' : true,
-            'plugins' : true
-        },
-        'document' : {
-            'hasStorageAccess' : true,
-            'requestStorageAccess' : true,
-            'cookie' : true
-        },
-        'screen' : {
-            'pixelDepth' : true,
-            'colorDepth' : true,
-            'height' : true,
-            'width' : true
-        },
-        'localStorage' : true,
-        'sessionStorage' : true
+if (Object.alreadyInjected === undefined)
+{
+    Object.alreadyInjected = true;
+    var monitorD = {
+        'window' : {
+            'navigator' : {
+                'appCodeName' : true,
+                'appName' : true,
+                'appVersion' : true,
+                'cookieEnabled' : true,
+                'geolocation' : true,
+                'language' : true,
+                'languages' : true,
+                'onLine' : true,
+                'oscpu' : true,
+                'platform' : true,
+                'product' : true,
+                'productSub' : true,
+                'userAgent' : true,
+                'vendorSub' : true,
+                'vendor' : true,
+                'plugins' : true
+            },
+            'document' : {
+                'hasStorageAccess' : true,
+                'requestStorageAccess' : true,
+                'cookie' : true
+            },
+            'screen' : {
+                'pixelDepth' : true,
+                'colorDepth' : true,
+                'height' : true,
+                'width' : true
+            },
+            'localStorage' : true,
+            'sessionStorage' : true
+        }
+    };
+
+    Object.getPropertyDescriptor = function (subject, name) {
+        var pd = Object.getOwnPropertyDescriptor(subject, name);
+        var proto = Object.getPrototypeOf(subject);
+        while (pd === undefined && proto !== null) {
+        pd = Object.getOwnPropertyDescriptor(proto, name);
+        proto = Object.getPrototypeOf(proto);
+        }
+        return pd;
+    };
+
+    function send_log(log_str) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("POST", 'http://1.255.54.63:10921/server', true);
+        xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+        xhr.send(log_str)
     }
-};
-
-Object.getPropertyDescriptor = function (subject, name) {
-    var pd = Object.getOwnPropertyDescriptor(subject, name);
-    var proto = Object.getPrototypeOf(subject);
-    while (pd === undefined && proto !== null) {
-    pd = Object.getOwnPropertyDescriptor(proto, name);
-    proto = Object.getPrototypeOf(proto);
-    }
-    return pd;
-};
-
-function send_log(log_str) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", 'http://1.255.54.63:10921/server', true);
-    xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xhr.send(log_str)
-}
 
 
-function instrumentProperty(object, objectName,  propertyName) {
-    var propDesc = Object.getPropertyDescriptor(object, propertyName);
+    function instrumentProperty(object, objectName,  propertyName) {
+        var propDesc = Object.getPropertyDescriptor(object, propertyName);
 
 
-    if(propDesc['configurable'] === true)
-    {
-        var originalGetter = propDesc.get;
-        var originalSetter = propDesc.set;
-        var originalValue = propDesc.value;
-
-        // We overwrite both data and accessor properties as an instrumented
-        // accessor property
-        Object.defineProperty(object, propertyName, {
-        configurable: true,
-        get: (function() {
-            return function() {
-            var origProperty;
-
-            // get original value
-            if (originalGetter) { // if accessor property
-                origProperty = originalGetter.call(this);
-            } else if ('value' in propDesc) { // if data property
-                origProperty = originalValue;
-            } else {
-                console.error("Property descriptor for",
-                    objectName + '.' + propertyName,
-                    "doesn't have getter or value?");
-                logValue(objectName + '.' + propertyName, "",
-                    "get(failed)", callContext, logSettings);
-                return;
-            }
-
-            send_log(objectName + '.' + propertyName + origProperty +
-                    "get");
-                return origProperty;
-            }
-        })(),
-        set: (function() {
-            return function(value) {
-            var returnValue;
-
-            // set new value to original setter/location
-            if (originalSetter) { // if accessor property
-                returnValue = originalSetter.call(this, value);
-            } else if ('value' in propDesc) { // if data property
-                originalValue = value;
-                returnValue = value;
-            } else {
-                console.error("Property descriptor for",
-                    objectName + '.' + propertyName,
-                    "doesn't have setter or value?");
-                logValue(objectName + '.' + propertyName, value,
-                    "set(failed)", callContext, logSettings);
-                return value;
-            }
-
-            // log set
-            send_log(objectName + '.' + propertyName + value +
-                "set");
-            // return new value
-            return returnValue;
-            }
-        })()
-        });
-    }
-    else
-    {
-        // TODO : implement monitoring for unconfigurable property
-        // monitor(object, propertyName);
-    }
-}
-
-function instrumentTree(object, base) {
-    for (var key in object)
-    {
-        try
+        if(propDesc['configurable'] === true)
         {
-            if (object[key] === true)
+            var originalGetter = propDesc.get;
+            var originalSetter = propDesc.set;
+            var originalValue = propDesc.value;
+
+            // We overwrite both data and accessor properties as an instrumented
+            // accessor property
+            Object.defineProperty(object, propertyName, {
+            configurable: true,
+            get: (function() {
+                return function() {
+                var origProperty;
+
+                // get original value
+                if (originalGetter) { // if accessor property
+                    origProperty = originalGetter.call(this);
+                } else if ('value' in propDesc) { // if data property
+                    origProperty = originalValue;
+                } else {
+                    console.error("Property descriptor for",
+                        objectName + '.' + propertyName,
+                        "doesn't have getter or value?");
+                    logValue(objectName + '.' + propertyName, "",
+                        "get(failed)", callContext, logSettings);
+                    return;
+                }
+
+                send_log(objectName + '.' + propertyName + origProperty +
+                        "get");
+                    return origProperty;
+                }
+            })(),
+            set: (function() {
+                return function(value) {
+                var returnValue;
+
+                // set new value to original setter/location
+                if (originalSetter) { // if accessor property
+                    returnValue = originalSetter.call(this, value);
+                } else if ('value' in propDesc) { // if data property
+                    originalValue = value;
+                    returnValue = value;
+                } else {
+                    console.error("Property descriptor for",
+                        objectName + '.' + propertyName,
+                        "doesn't have setter or value?");
+                    logValue(objectName + '.' + propertyName, value,
+                        "set(failed)", callContext, logSettings);
+                    return value;
+                }
+
+                // log set
+                send_log(objectName + '.' + propertyName + value +
+                    "set");
+                // return new value
+                return returnValue;
+                }
+            })()
+            });
+        }
+        else
+        {
+            // TODO : implement monitoring for unconfigurable property
+            // monitor(object, propertyName);
+        }
+    }
+
+    function instrumentTree(object, base) {
+        for (var key in object)
+        {
+            try
             {
-                instrumentProperty(eval(base), base, key);
+                if (object[key] === true)
+                {
+                    instrumentProperty(eval(base), base, key);
+                }
+                else if (object[key] instanceof Object) {
+                    instrumentTree(object[key], base + "." + key);
+                }
             }
-            else if (object[key] instanceof Object) {
-                instrumentTree(object[key], base + "." + key);
+            catch (err)
+            {
             }
-        }
-        catch (err)
-        {
         }
     }
-}
 
-instrumentTree(monitorD['window'], 'window');
+    instrumentTree(monitorD['window'], 'window');
+}
