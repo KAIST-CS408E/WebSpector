f1c6949abe8d1a727c9651bd34625501ce235316
Reinose <sutt69@kaist.ac.kr>
2018-10-04 16:00:43 +0900

    Update proxy_worker to interact with DB server
    * Convert multi-process to multi-thread to use DB connection pool

--- backend/proxy_worker.Dockerfile
+++ backend/proxy_worker.Dockerfile
@@ -4,4 +4,17 @@ MAINTAINER Gyeongwon Kim <sutt69@kaist.ac.kr>
 RUN sed -i 's/archive.ubuntu.com/kr.archive.ubuntu.com/g' /etc/apt/sources.list
 RUN apt-get clean && apt-get update
 
-CMD ["/bin/bash"]
+RUN useradd -ms /bin/bash worker
+
+RUN apt-get install -y python3 python3-pip
+
+#ADD ./proxy_worker /home/worker/proxy_worker
+#RUN chown -R worker:worker /home/worker/proxy_worker
+#RUN pip3 install -r /home/worker/proxy_worker/requirements.txt
+
+ADD ./proxy_worker/requirements.txt /
+RUN pip3 install -r /requirements.txt
+
+USER worker
+WORKDIR /home/worker/proxy_worker
+
--- backend/proxy_worker/main.py
+++ backend/proxy_worker/main.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 
 import json
+import os
+import pymysql
 
+from DBUtils.PooledDB import PooledDB, TooManyConnections
 from http.server import HTTPServer, BaseHTTPRequestHandler
-from socketserver import ForkingMixIn, TCPServer
+from socketserver import ThreadingMixIn, TCPServer
 
 class WorkerHandler(BaseHTTPRequestHandler):
     def do_POST(self):
+        global pool
         if 'Content-Length' not in self.headers:
             self.send_response(400, "Bad Request")
             self.send_header("Content-type", "text/html")
@@ -33,10 +37,17 @@ class WorkerHandler(BaseHTTPRequestHandler):
         raw_data = self.rfile.read(content_length)
         try:
             json_data = json.loads(raw_data)
-            print(json_data)
+            db = pool.connection()
+            cur = db.cursor()
+            cur.execute("INSERT INTO test (json) VALUES ('{}')".format(raw_data.decode("UTF-8")))
+            db.commit()
+            del cur
+            del db
             payload = """{"error":0,"message":"Success"}"""
         except json.decoder.JSONDecodeError:
             payload = '{"error":1,"message":"Illegal JSON data"}'
+        except TooManyConnections:
+            payload = '{"error":255,"message":"Too much traffic"}'
 
         self.send_response(200, "Success")
         self.send_header("Content-type", "application/json")
@@ -44,14 +55,31 @@ class WorkerHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(payload, "UTF-8"))
 
-class ForkHTTPServer(ForkingMixIn, TCPServer):
+class ThreadedHTTPServer(ThreadingMixIn, TCPServer):
     allow_reuse_address = True
 
+
+def init():
+    global pool
+    host = os.environ.get("DB_HOST", "192.168.0.255")
+    port = int(os.environ.get("DB_PORT", "3306"))
+    dbname = os.environ.get("DB_DATABASE", "webspector")
+    user = os.environ.get("DB_USER", "dbadmin")
+    passwd = os.environ.get("DB_PASSWORD", "dbadminpassword")
+
+    pool = PooledDB(pymysql, mincached=4, host=host, port=port, db=dbname, \
+                    user=user, passwd=passwd)
+
+def fini():
+    global pool
+    del pool
 
 def run(port=55555, server_class=HTTPServer, handler_class=WorkerHandler):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
+    init()
     try:
+        print("[*] Server Started")
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
@@ -64,7 +92,10 @@ def run(port=55555, server_class=HTTPServer, handler_class=WorkerHandler):
         exit(1)
     httpd.shutdown()
     httpd.server_close()
+    print("")
+    print("[*] Server Ended")
+    fini()
 
 
 if __name__ == "__main__":
-    run(server_class=ForkHTTPServer)
+    run(int(os.environ.get("PROXY_WORKER_PORT", "55555")), server_class=ThreadedHTTPServer)
