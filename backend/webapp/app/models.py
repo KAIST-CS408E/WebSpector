ceb54a77dc396717218db06b52448f24113bc65c
okas832 <okas832@kaist.ac.kr>
2018-11-26 04:37:52 +0900

    Change model types

--- backend/webapp/app/models.py
+++ backend/webapp/app/models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean
+from sqlalchemy import Column, Integer, DateTime, Text, JSON
 from . import Base
 
 class Inspect(Base):
     id = Column(Integer, primary_key=True)
-    name = Column(String(4000), unique = False)
-    property = Column(String(4000), unique = False)
-    location = Column(String(4000), unique = False) 
-    trace = Column(String(4000), unique = False)
+    name = Column(Text, unique = False)
+    property = Column(JSON, unique = False)
+    location = Column(Text, unique = False) 
+    trace = Column(Text, unique = False)
     time = Column(DateTime, unique = False)
 
     def __init__(self, name, property, location, trace, time):
