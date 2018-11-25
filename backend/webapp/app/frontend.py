0f5c716c4a5a216c186938c3028a838ea10e6321
okas832 <okas832@kaist.ac.kr>
2018-11-26 04:37:31 +0900

    Implement web structure

--- backend/webapp/app/frontend.py
+++ backend/webapp/app/frontend.py
@@ -1,19 +1,29 @@
 from flask import Blueprint, render_template, url_for, redirect, flash, request, abort, send_from_directory
 from . import db_session
 from .models import *
+
+import requests
 import json
 
 frontend = Blueprint('frontend', __name__)
 
 @frontend.route("/")
 def index():
-    return render_template("index.html")
+    return render_template('index.html.j2', search='http://www.example.com/')
 
-@frontend.route("/api/inspect/<url>")
-def inspect():
-    return
+@app.route('/search', methods=['GET'])
+def search():
+    url = str()
+    if 'key' in request.args:
+        url = request.args.get('key')
+    return render_template('search.html.j2', url=url)
 
-@frontend.route("/api/result/<reqkey>")
-def inspect():
-    return
+@app.route('/api/result', methods=["GET"])
+def result():
+    time.sleep(1)
+    url = str()
+    if 'url' in request.args:
+        url = request.args.get('url')
+    return '{"url": "' + url + '"}'
+
 
