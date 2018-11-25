d22b0663a2775e00e4872c0da7f2517b35b826d8
Reinose <sutt69@kaist.ac.kr>
2018-11-26 00:19:26 +0900

    Add tester

---
+++ tester/tester.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2
+
+from pwn import remote, p32
+
+import time
+import sys
+
+def main(file):
+    try:
+        with open(file, "r") as f:
+            websites = f.readlines()
+
+        count = 0
+        for website in websites:
+            if count > 5:
+                time.sleep(5)
+            for browser in ["\x01", "\x02"]:
+                p = remote("ec2-54-180-94-105.ap-northeast-2.compute.amazonaws.com", 31333)
+                payload = "\xff"
+                payload += browser
+                payload += p32(len(website), endian="big")
+                payload += website
+                p.send(payload)
+                p.close()
+
+            for browser in ["\x01", "\x02", "\x03"]:
+                p = remote("ec2-13-125-153-202.ap-northeast-2.compute.amazonaws.com", 31333)
+                payload = "\xff"
+                payload += browser
+                payload += p32(len(website), endian="big")
+                payload += website
+                p.send(payload)
+                p.close()
+
+            for browser in ["\x01", "\x02", "\x04"]:
+                p = remote("143.248.2.114", 31333)
+                payload = "\xff"
+                payload += browser
+                payload += p32(len(website), endian="big")
+                payload += website
+                p.send(payload)
+                p.close()
+            count += 1
+    except IOError as e:
+        print >> sys.stderr, str(e)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print >> sys.stderr, "Usage: {} <website list file>".format(sys.argv[0])
+        exit(1)
+    main(sys.argv[1])
